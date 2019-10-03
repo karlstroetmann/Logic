17fe1f49e154b17cd93836ffaac2ac31fb9f4020
karlstroetmann <karl.stroetmann@dhbw-mannheim.de>
2019-10-03 23:16:06 +0200

    minor edits

--- Python/min_sort.py
+++ Python/min_sort.py
@@ -4,7 +4,8 @@ def minSort(L):
     m = min(L)
     return [m] + minSort([x for x in L if x != m])
 
-L = [ 2, 13, 5, 13, 7, 2, 4 ]
-print('minSort(', L, ') = ', minSort(L), sep='')
+if __name__ == '__main__':
+    L = [ 2, 13, 5, 13, 7, 2, 4 ]
+    print('minSort(', L, ') = ', minSort(L), sep='')
 
 
