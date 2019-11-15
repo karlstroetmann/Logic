46ef8e1694322d02c02db36ca17950b8837e2d65
karlstroetmann <karl.stroetmann@dhbw-mannheim.de>
2019-11-15 21:03:39 +0100

    minor edits

--- Python/davisPutnam.py
+++ Python/davisPutnam.py
@@ -1,12 +1,12 @@
 def complement(l):
-    "Compute the complement of the literal l."
+    "Compute the complement of the literal L."
     if isinstance(l, str):
         return ('¬', l)
     else:
         return l[1]
 
 def extractVariable(l):
-    "Extract the propositional variable from the literal l."
+    "Extract the propositional variable from the literal L."
     if isinstance(l, str):
         return l
     else:
@@ -17,21 +17,20 @@ def arb(S):
     for x in S:
         return x
 
-def selectLiteral(Clauses, Forbidden):
-    Variables = { extractVariable(l) for C in Clauses for l in C } - Forbidden
-    return arb(Variables)
+def selectVariable(Clauses, Forbidden):
+    return arb({ extractVariable(L) for C in Clauses for L in C } - Forbidden)
 
 def reduce(Clauses, l):
     lBar = complement(l)
-    return   { C - { lBar } for C in Clauses if lBar in C }          \
-           | { C for C in Clauses if lBar not in C and l not in C }  \
+    return   { C - { lBar } for C in Clauses if lBar in C }         \
+           | { C for C in Clauses if lBar not in C and l not in C } \
            | { frozenset({l}) }
 
 def saturate(Clauses):
     S     = Clauses.copy()
-    Units = { C for C in S if len(C) == 1 }
+    Units = { C for C in S if len(C) == 1 } # set of unit clausesoccurring in C
     Used  = set()                           # remember which unit clauses have already been used
-    while len(Units) > 0:
+    while len(Units) > 0:  # iterate as long as we derive new unit clauses
         unit  = Units.pop()
         Used |= { unit }
         l     = arb(unit)
@@ -39,23 +38,24 @@ def saturate(Clauses):
         Units = { C for C in S if len(C) == 1 } - Used        
     return S
 
-def solve(Clauses, Literals):
+def solve(Clauses, Variables):
     S      = saturate(Clauses);
     empty  = frozenset()
     Falsum = {empty}
-    if empty in S:
-        return Falsum
-    if all(len(C) == 1 for C in S):
+    if empty in S:                  # S is inconsistent
+        return Falsum               
+    if all(len(C) == 1 for C in S): # S is trivial
         return S
-    l      = selectLiteral(S, Literals)
-    negL   = complement(l)
-    Result = solve(S | { frozenset({l}) }, Literals | { l })
+    # case distinction on variaable p
+    p      = selectVariable(S, Variables)
+    negP   = complement(p)
+    Result = solve(S | { frozenset({p}) }, Variables | { p })
     if Result != Falsum:
         return Result
-    return solve(S | { frozenset({negL}) }, Literals | { l })
+    return solve(S | { frozenset({negP}) }, Variables| { p })
 
 def toString(S):
-    "convert the set S of frozen sets to a string."
+    "Convert the set S of frozen sets to a string where frozen sets are written as sets."
     if S == set():
         return '{}'
     result = '{ '
@@ -64,17 +64,9 @@ def toString(S):
     result = result[:-2]
     result += ' }'
     return result
+    
 
-if __name__ == "__main__":
-    c1 = frozenset({ 'r', 'p', 's' })
-    c2 = frozenset({ 'r', 's' })
-    c3 = frozenset({ 'p', 'q', 's' })
-    c4 = frozenset({ ('¬', 'p'), ('¬', 'q') })
-    c5 = frozenset({ ('¬', 'p'), 's', ('¬', 'r') })
-    c6 = frozenset({ 'p', ('¬', 'q'), 'r'})
-    c7 = frozenset({ ('¬', 'r'), ('¬', 's'), 'q' })
-    c8 = frozenset({ ('¬', 'p'), ('¬', 's')})
-    c9 = frozenset({ 'p', ('¬', 'r'), ('¬', 'q') })
-    c0 = frozenset({ ('¬', 'p'), 'r', 'q', ('¬', 's') })
-    S  = { c0, c1, c2, c3, c4, c5, c6, c7, c8, c9 }
-    print(toString(solve(S, set())))
+
+
+
+
