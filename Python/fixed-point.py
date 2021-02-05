ce2583be6d847ad477a32665064707b74eedf68a
Pius Walter <pius.walter@gmx.net>
2021-02-05 20:24:59 +0100

    Removed unnecessary semicolons

--- Python/fixed-point.py
+++ Python/fixed-point.py
@@ -6,10 +6,10 @@ def solve(f, x0):
     """
     x = x0
     for n in range(10000):  # at most 10000 iterations
-        oldX = x;
-        x    = f(x);
+        oldX = x
+        x    = f(x)
         if abs(x - oldX) < 1.0e-15: 
-            return x;
+            return x
 
-print("solution to x = cos(x): ", solve(cos, 0));
-print("solution to x = 1/(1+x):", solve(lambda x: 1/(1+x), 0));
+print("solution to x = cos(x): ", solve(cos, 0))
+print("solution to x = 1/(1+x):", solve(lambda x: 1/(1+x), 0))
