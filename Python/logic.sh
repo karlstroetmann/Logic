cc2367c9dd1eb4d378c93cf6cb1f02b5f76a7f72
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2021-10-20 15:28:44 +0200

    logic.sh added

---
+++ Python/logic.sh
@@ -0,0 +1,6 @@
+conda create -n logic python=3 jupyter notebook
+conda activate logic
+conda install -c anaconda matplotlib
+conda install -c anaconda graphviz
+conda install -c conda-forge python-graphviz
+conda install -c conda-forge ipycanvas
