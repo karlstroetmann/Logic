eb70d47eea1fa78d4927c07711b703ac1de19d4f
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2023-03-30 16:54:12 +0200

    some restructuring

--- Python/logic.sh
+++ Python/logic.sh
@@ -1,6 +1,4 @@
-conda create -n logic python=3 jupyter notebook
+conda create -n logic python=3.10 jupyter notebook
 conda activate logic
-conda install -c anaconda matplotlib
-conda install -c anaconda graphviz
-conda install -c conda-forge python-graphviz
-conda install -c conda-forge ipycanvas
+conda install -c anaconda matplotlib graphviz
+conda install -c conda-forge python-graphviz ipycanvas
