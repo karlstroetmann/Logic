de58f625d7ba96edf2fc3abe4044bec96d51689b
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2024-04-08 00:49:43 +0200

    minor changes

--- Python/logic.sh
+++ Python/logic.sh
@@ -1,6 +1,7 @@
 conda create -n logic python=3.12 jupyter notebook
+conda create -n logic python=3.12 jupyter notebook
 conda activate logic
 conda install -c anaconda matplotlib graphviz
 conda install -c conda-forge python-graphviz ipycanvas
-python3 -m pip install nb_mypy
+pip install nb_mypy
 pip install z3-solver
