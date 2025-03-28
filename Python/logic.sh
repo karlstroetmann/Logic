bf9aab040e202f8c4d8cf63955f8421f28ba76cf
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2025-03-28 12:47:30 +0100

    .gitignore updated

--- Python/logic.sh
+++ Python/logic.sh
@@ -1,5 +1,4 @@
-conda create -n logic python=3.12 jupyter notebook
-conda create -n logic python=3.12 jupyter notebook
+conda create -n logic -c conda-forge python=3.13 jupyter_server nbclassic
 conda activate logic
 conda install -c anaconda matplotlib graphviz
 conda install -c conda-forge python-graphviz ipycanvas
