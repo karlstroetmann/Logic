6378acf93911175ccda49af5ba2cc8f2f2e3fccb
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2026-01-19 01:51:14 +0100

    added TypeScript directory

--- Python/logic.sh
+++
@@ -1,6 +0,0 @@
-conda create -n logic -c conda-forge python=3.13 jupyter_server nbclassic
-conda activate logic
-conda install -c anaconda matplotlib graphviz
-conda install -c conda-forge python-graphviz ipycanvas
-pip install nb_mypy
-pip install z3-solver
--- Python/min_sort.py
+++
@@ -1,9 +0,0 @@
-def minSort(L):
-    if L == []:
-        return []
-    m = min(L)
-    return [m] + minSort([x for x in L if x != m])
-
-if __name__ == '__main__':
-    L = [ 2, 13, 5, 13, 7, 2, 4 ]
-    print('minSort(', L, ') = ', minSort(L), sep='')
